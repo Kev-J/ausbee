8d6efdeeaf2d0584ecd5067aaf649b3e750b9131
Kevin_JOLY <joly.kevin25@gmail.com>
2013-11-24 14:34:51 +0100

    [SOFTWARE] Remove packages directory

--- Software/packages/config.mk
+++
@@ -1,6 +0,0 @@
-include packages/libausbee/config.mk
-include packages/FreeRTOS/config.mk
-
-# Mandatory packages
-PACKAGES_TARGET += $(FREERTOS_DONE)
-PACKAGES_TARGET += $(LIBAUSBEE_DONE) #TODO make it not mandatory
--- Software/packages/packages.mk
+++
@@ -1,3 +0,0 @@
-# Include packages makefiles
-include $(PACKAGES_PATH)/FreeRTOS/FreeRTOS.mk
-include $(PACKAGES_PATH)/libausbee/libausbee.mk
