d0a380df99612e7e30021cb7d3a25bab0efba1fa
Kevin_JOLY <joly.kevin25@gmail.com>
2013-11-30 14:25:40 +0100

    [SOFTWARE][LIBAUSBEE] Add device.h header for defining STM32 serie

---
+++ Software/Libraries/libausbee/include/AUSBEE/device.h
@@ -0,0 +1,18 @@
+#ifndef AUSBEE_DEVICE_H
+#define AUSBEE_DEVICE_H
+
+#if defined (STM32F40_41xxx) || defined (STM32F427_437xx) || defined (STM32F429_439xx) || defined (STM32F401xx)
+
+#define STM32F4XX /* STM32 F4 serie */
+
+#elif defined (STM32F10X_LD) || defined (STM32F10X_LD_VL) || defined (STM32F10X_MD) || defined (STM32F10X_MD_VL) || defined (STM32F10X_HD) || defined (STM32F10X_HD_VL) || defined (STM32F10X_XL) || defined (STM32F10X_CL)
+
+#define /* STM32 F1 serie */
+
+#else
+
+#error "Undefined or unsupported STM32 device for libausbee"
+
+#endif /* STM32FXXX */
+
+#endif /* AUSBEE_DEVICE_H */
