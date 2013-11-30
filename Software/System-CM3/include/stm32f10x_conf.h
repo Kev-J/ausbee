5f80b42bc88e9cd6e963986ca11cf46596f79954
Kevin_JOLY <joly.kevin25@gmail.com>
2013-11-30 12:01:05 +0100

    [SOFTWARE] Make the assert system dependant to DEBUG macro

--- Software/System-CM3/include/stm32f10x_conf.h
+++ Software/System-CM3/include/stm32f10x_conf.h
@@ -1,6 +1,6 @@
 #include <stm32f10x.h>
 
-#ifdef USE_ASSERT_PARAM
+#ifdef DEBUG
 #define assert_param(x) ((x) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
 void assert_failed((uint8_t *filename, uint32_t line);
 #else
