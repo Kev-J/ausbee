b70d0293265d0531c5c6f99341ffa9417206111e
Kevin JOLY <joly.kevin25@gmail.com>
2013-12-03 19:31:08 +0100

    [SOFTWARE][SYSTEM-CM4] Remove a bracket

--- Software/System-CM4/include/stm32f4xx_conf.h
+++ Software/System-CM4/include/stm32f4xx_conf.h
@@ -2,7 +2,7 @@
 
 #ifdef DEBUG
 #define assert_param(x) ((x) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
-void assert_failed((uint8_t *filename, uint32_t line);
+void assert_failed(uint8_t *filename, uint32_t line);
 #else
 #define assert_param(x) ((void)0)
 #endif
