e307dbfe1dd77ad4b5061dcc86d9cdc5f046f49b
David Bitonneau <david.bitonneau@gmail.com>
2014-05-01 18:31:18 +0200

    [SOFT] Improving ploting tool

--- Software/Libraries/libausbee/tools/pc/plot/anim_plot.py
+++ Software/Libraries/libausbee/tools/pc/plot/anim_plot.py
@@ -12,52 +12,41 @@ x = np.arange(0, x_range, 1)        # x-array
 
 ymin = -1000
 ymax = 3000
-line,  = ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
-line2, = ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
-line3, = ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
-line4, = ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
-measure_text   = ax.text(0.02, 0.95, '', transform=ax.transAxes)
-error_text     = ax.text(0.02, 0.90, '', transform=ax.transAxes)
-error_sum_text = ax.text(0.02, 0.85, '', transform=ax.transAxes)
-command_text   = ax.text(0.02, 0.80, '', transform=ax.transAxes)
+lines = []
+lines_text = []
 
 values=[]
-values2=[]
-values3=[]
-values4=[]
 
-def animate(i):
+def animate(num):
     lineread = ser.readline().split(",")
-    val  = int(lineread[0])
-    val2 = int(lineread[1])
-    val3 = int(lineread[2])
-    val4 = int(lineread[3])
-    values.append(val)
-    values2.append(val2)
-    values3.append(val3/10)
-    values4.append(val4*10)
-    if i>x_range:
-        line.set_ydata(values[-x_range:])  # update the measure
-        line2.set_ydata(values2[-x_range:])  # update the error
-        line3.set_ydata(values3[-x_range:])  # update the error sum
-        line4.set_ydata(values4[-x_range:])  # update the command
-    measure_text.set_text('measure = %.1f' % val)
-    error_text.set_text('error = %.1f' % val2)
-    error_sum_text.set_text('error sum = %.1f' % val3)
-    command_text.set_text('command = %.1f' % val4)
-    return line, line2, line3, line4, measure_text, error_text, error_sum_text, command_text
+    for i in range(len(lineread)):
+        val_name, val, scale = lineread[i].split(":")
+        val = int(val)
+        scale = float(scale)
+        lines_text[i].set_text('%s = %.1f' % (val_name, val))
+        values[i].append(val * scale)
+        if num>x_range:
+            lines[i].set_ydata(values[i][-x_range:])  # update the measure
+    l = lines[:]
+    l.extend(lines_text)
+    return l
 
 #Init only required for blitting to give a clean slate.
 def init():
-    line.set_ydata(np.ma.array(x, mask=True))
-    line2.set_ydata(np.ma.array(x, mask=True))
-    line3.set_ydata(np.ma.array(x, mask=True))
-    line4.set_ydata(np.ma.array(x, mask=True))
-    measure_text.set_text('')
-    error_text.set_text('')
-    error_sum_text.set_text('')
-    command_text.set_text('')
-    return line, line2, line3, line4, measure_text, error_text, error_sum_text, command_text
+    for i in range(4):
+        values.append([])
+
+        line, = ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
+        line.set_ydata(np.ma.array(x, mask=True))
+        lines.append(line)
+
+        line_text = ax.text(0.02, 0.95-i*0.05, '', transform=ax.transAxes)
+        line_text.set_text('')
+        lines_text.append(line_text)
+
+    l = lines[:]
+    l.extend(lines_text)
+    return l
 
 ani = animation.FuncAnimation(fig, animate, np.arange(1, 200000), init_func=init,
     interval=25, blit=True)
