3e5f62e301f05e54e398059e4735996891e2a46e
David Bitonneau <david.bitonneau@gmail.com>
2014-05-02 20:15:52 +0200

    [SOFT] Ploting pid values for both motors

--- Software/Libraries/libausbee/tools/pc/plot/anim_plot.py
+++ Software/Libraries/libausbee/tools/pc/plot/anim_plot.py
@@ -6,29 +6,54 @@ ser = serial.Serial('/dev/ttyUSB0', 115200)
 
 x_range = 50
 
-fig, ax = plt.subplots()
+fig = plt.figure()
+
+right_ax = fig.add_subplot(1, 2, 1)
+right_values = []
+right_lines = []
+right_lines_text = []
+
+left_ax = fig.add_subplot(1, 2, 2)
+left_values = []
+left_lines = []
+left_lines_text = []
+
+num_lines = 1
 
 x = np.arange(0, x_range, 1)        # x-array
 
 ymin = -1000
 ymax = 3000
-values = []
-lines = []
-lines_text = []
-num_lines = 1
 
 def animate(num):
+    # Right motor
     lineread = ser.readline().split(",")
     for i in range(len(lineread)):
         val_name, val, scale = lineread[i].split(":")
         val = int(val)
         scale = float(scale)
-        lines_text[i].set_text('%s = %.1f' % (val_name, val))
-        values[i].append(val * scale)
+        right_lines_text[i].set_text('%s = %.1f' % (val_name, val))
+        right_values[i].append(val * scale)
         if num>x_range:
-            lines[i].set_ydata(values[i][-x_range:])  # update the measure
-    l = lines[:]
-    l.extend(lines_text)
+            right_lines[i].set_ydata(right_values[i][-x_range:])  # update the measure
+
+    l = right_lines[:]
+    l.extend(right_lines_text)
+
+    # Left motor
+    lineread = ser.readline().split(",")
+    for i in range(len(lineread)):
+        val_name, val, scale = lineread[i].split(":")
+        val = int(val)
+        scale = float(scale)
+        left_lines_text[i].set_text('%s = %.1f' % (val_name, val))
+        left_values[i].append(val * scale)
+        if num>x_range:
+            left_lines[i].set_ydata(left_values[i][-x_range:])  # update the measure
+
+    l.extend(left_lines[:])
+    l.extend(left_lines_text)
+
     return l
 
 def init():
@@ -36,24 +61,36 @@ def init():
     while not ser.readline():
         print("")
     for i in range(len(ser.readline().split(","))):
-        values.append([])
+        right_values.append([])
+        left_values.append([])
 
-        line, = ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
-        lines.append(line)
+        line, = right_ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
+        right_lines.append(line)
+        line, = left_ax.plot(x, (ymax-ymin)/2*(np.sin(x))-ymin, 'o-')
+        left_lines.append(line)
 
-        line_text = ax.text(0.02, 0.95-i*0.05, '', transform=ax.transAxes)
-        lines_text.append(line_text)
+        line_text = right_ax.text(0.02, 0.95-i*0.05, '', transform=right_ax.transAxes)
+        right_lines_text.append(line_text)
+        line_text = left_ax.text(0.02, 0.95-i*0.05, '', transform=left_ax.transAxes)
+        left_lines_text.append(line_text)
 
     num_lines = i+1
 
 #Reset only required for blitting to give a clean slate.
 def reset():
     for i in range(num_lines):
-        lines[i].set_ydata(np.ma.array(x, mask=True))
-        lines_text[i].set_text('')
+        right_lines[i].set_ydata(np.ma.array(x, mask=True))
+        right_lines_text[i].set_text('')
 
-    l = lines[:]
-    l.extend(lines_text)
+        left_lines[i].set_ydata(np.ma.array(x, mask=True))
+        left_lines_text[i].set_text('')
+
+    l = right_lines[:]
+    l.extend(right_lines_text)
+
+    l.extend(left_lines[:])
+    l.extend(left_lines_text)
+
     return l
 
 init()
