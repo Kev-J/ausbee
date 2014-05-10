2cc1de68b0eda9f6b2b419059e23b2be207d3642
David Bitonneau <david.bitonneau@gmail.com>
2014-05-10 17:22:11 +0200

    [SOFT] plotting tool: adding robot position on table

--- Software/Libraries/libausbee/tools/pc/plot/anim_plot.py
+++ Software/Libraries/libausbee/tools/pc/plot/anim_plot.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import serial
+from subplot import Subplot, SubplotTable
 
 if len(sys.argv) == 1:
     ser = serial.Serial('/dev/ttyUSB0', 115200)
@@ -14,70 +15,10 @@ else:
 fig = plt.figure()
 x_range = 50
 
-class Subplot:
-    def __init__(self, _ymin, _ymax, _x_range, fig, *args, **kwargs):
-        self.ax = fig.add_subplot(*args)
-        self.values = []
-        self.lines = []
-        self.lines_text = []
-        self.num_lines = 0
-        self.ymin = _ymin
-        self.ymax = _ymax
-        self.x_range = _x_range
-        self.x = np.arange(0, x_range, 1)        # x-array
-        self.dynamic_init(1)
-
-    def dynamic_init(self, new_size):
-        if new_size > self.num_lines:
-            for i in range(new_size - self.num_lines):
-                self.values.append([])
-
-                line, = self.ax.plot(self.x, (self.ymax-self.ymin)/2*(np.sin(self.x))-self.ymin, 'o-')
-                self.lines.append(line)
-
-                line_text = self.ax.text(0.02, 0.95 - (self.num_lines + i) * 0.05, '', transform=self.ax.transAxes)
-                self.lines_text.append(line_text)
-
-            self.num_lines = new_size
-
-    def update_lines(self, frame_num, lineread):
-        splited_read = lineread.split(";")
-        self.dynamic_init(len(splited_read))
-        for i in range(self.num_lines):
-            try:
-                val_name, val, scale = splited_read[i].split(":")
-                val = float(val)
-                scale = float(scale)
-                self.lines_text[i].set_text('%s = %.1f' % (val_name, val))
-                self.values[i].append(val * scale)
-            except ValueError:
-                self.values[i].append(0)
-                print("[ValueError] Input: \"" + splited_read[i] + "\"")
-            if frame_num > self.x_range:
-                ydata = self.values[i][-x_range:]
-            else:
-                ydata = np.zeros(x_range)
-                ydata[-frame_num:] = self.values[i][-frame_num:]
-            self.lines[i].set_ydata(ydata)  # update the measure
-
-        l = self.lines[:]
-        l.extend(self.lines_text)
-
-        return l
-
-    def reset(self):
-        for i in range(self.num_lines):
-            self.lines[i].set_ydata(np.ma.array(self.x, mask=True))
-            self.lines_text[i].set_text('')
-
-        l = self.lines[:]
-        l.extend(self.lines_text)
-
-        return l
-
 right_motor_subplot = Subplot(-1000, 3000, x_range, fig, 2, 2, 2)
 left_motor_subplot = Subplot(-100, 3000, x_range, fig, 2, 2, 4)
-robot_subplot = Subplot(-200, 1200, x_range, fig, 1, 2, 1)
+odometry_subplot = Subplot(-200, 1200, x_range, fig, 2, 2, 1)
+robot_subplot = SubplotTable(fig, 2, 2, 3)
 
 def readline_starting_with(start_string):
     c = 'a'
@@ -99,6 +40,7 @@ def readline_starting_with(start_string):
 def animate(frame_num):
     l = right_motor_subplot.update_lines(frame_num, readline_starting_with("Right"))
     l.extend(left_motor_subplot.update_lines(frame_num, readline_starting_with("Left")))
+    l.extend(odometry_subplot.update_lines(frame_num, readline_starting_with("Odometry")))
     l.extend(robot_subplot.update_lines(frame_num, readline_starting_with("Robot")))
     return l
 
@@ -106,6 +48,7 @@ def animate(frame_num):
 def reset():
     l = right_motor_subplot.reset()
     l.extend(left_motor_subplot.reset())
+    l.extend(odometry_subplot.reset())
     l.extend(robot_subplot.reset())
     return l
 
---
+++ Software/Libraries/libausbee/tools/pc/plot/subplot.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Subplot:
+    def __init__(self, _ymin, _ymax, _x_range, fig, *args, **kwargs):
+        self.ax = fig.add_subplot(*args)
+        self.values = []
+        self.lines = []
+        self.lines_text = []
+        self.num_lines = 0
+        self.ymin = _ymin
+        self.ymax = _ymax
+        self.x_range = _x_range
+        self.x = np.arange(0, self.x_range, 1)        # x-array
+        self.dynamic_init(1)
+
+    def dynamic_init(self, new_size):
+        if new_size > self.num_lines:
+            for i in range(new_size - self.num_lines):
+                self.values.append([])
+
+                line, = self.ax.plot(self.x, (self.ymax-self.ymin)/2*(np.sin(self.x))-self.ymin, 'o-')
+                self.lines.append(line)
+
+                line_text = self.ax.text(0.02, 0.95 - (self.num_lines + i) * 0.05, '', transform=self.ax.transAxes)
+                self.lines_text.append(line_text)
+
+            self.num_lines = new_size
+
+    def update_lines(self, frame_num, lineread):
+        splited_read = lineread.split(";")
+        self.dynamic_init(len(splited_read))
+        for i in range(self.num_lines):
+            try:
+                val_name, val, scale = splited_read[i].split(":")
+                val = float(val)
+                scale = float(scale)
+                self.lines_text[i].set_text('%s = %.1f' % (val_name, val))
+                self.values[i].append(val * scale)
+            except ValueError:
+                self.values[i].append(0)
+                print("[ValueError] Input: \"" + splited_read[i] + "\"")
+            if frame_num > self.x_range:
+                ydata = self.values[i][-self.x_range:]
+            else:
+                ydata = np.zeros(self.x_range)
+                ydata[-frame_num:] = self.values[i][-frame_num:]
+            self.lines[i].set_ydata(ydata)  # update the measure
+
+        l = self.lines[:]
+        l.extend(self.lines_text)
+
+        return l
+
+    def reset(self):
+        for i in range(self.num_lines):
+            self.lines[i].set_ydata(np.ma.array(self.x, mask=True))
+            self.lines_text[i].set_text('')
+
+        l = self.lines[:]
+        l.extend(self.lines_text)
+
+        return l
+
+class SubplotTable:
+    def __init__(self, fig, *args, **kwargs):
+        self.ax = fig.add_subplot(autoscale_on=False, xlim=(-750, 750), ylim=(-750, 750), *args)
+        self.lines_x = []
+        self.lines_y = []
+
+        self.line, = self.ax.plot([], [], 'o-', lw=2)
+        self.x_text = self.ax.text(0.05, 0.95, '', transform=self.ax.transAxes)
+        self.y_text = self.ax.text(0.05, 0.9, '',  transform=self.ax.transAxes)
+
+    def update_lines(self, frame_num, lineread):
+        splited_read = lineread.split(";")
+
+        x_name, x = splited_read[0].split(":")
+        x = float(x)
+        y_name, y = splited_read[1].split(":")
+        y = float(y)
+
+        self.x_text.set_text('%s = %.1f' % (x_name, x))
+        self.y_text.set_text('%s = %.1f' % (y_name, y))
+
+        self.lines_x.append(x)
+        self.lines_y.append(y)
+
+        self.line.set_data(self.lines_x, self.lines_y)
+
+        return self.line, self.x_text, self.y_text
+
+    def reset(self):
+        self.line.set_data([], [])
+        self.x_text.set_text('')
+        self.y_text.set_text('')
+
+        return self.line, self.x_text, self.y_text
