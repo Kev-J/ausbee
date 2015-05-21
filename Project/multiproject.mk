9992c2a7f7b68678a52d3b57a81267652d19f8ac
Damien Espitallier <damien.espitallier@gmail.com>
2015-05-21 22:54:14 +0200

    generate project object file in Output/Project directory

--- Project/multiproject.mk
+++ Project/multiproject.mk
@@ -1,5 +1,7 @@
+PROJECT_OUTPUT_PATH=$(OUTPUT_PATH)/Project
+
 #Generate object list from project source file list
-PROJECT_OBJ_FILES=$(patsubst %.c,%.o,${PROJECT_SRC_FILES})
+PROJECT_OBJ_FILES=$(patsubst %.c,${PROJECT_OUTPUT_PATH}/%.o,${PROJECT_SRC_FILES})
 
 #Add project object file to global object file list
 OBJ_FILES+=$(PROJECT_OBJ_FILES)
@@ -11,5 +13,6 @@ PROJECT_INCLUDES += $(PACKAGES_INCLUDES)
 PROJECT_INCLUDES += $(SYSTEM_INCLUDES)
 
 #Generate project object files
-$(PROJECT_OBJ_FILES): %.o: %.c $(PACKAGES_EXTRACTED) $(TOOLCHAIN_EXTRACTED)
+$(PROJECT_OBJ_FILES): ${PROJECT_OUTPUT_PATH}/%.o: %.c $(PACKAGES_EXTRACTED) $(TOOLCHAIN_EXTRACTED)
+	@mkdir -p $(dir $@)
 	$(HOST_CC) -o $@ $(HOST_CFLAGS) $(PROJECT_INCLUDES) $(HOST_OPTIMISATION) -c $<
