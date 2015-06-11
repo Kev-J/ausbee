eff32fc95d6821e9c5b1af0b81b4b336c1addf67
Kevin JOLY <joly.kevin25@gmail.com>
2015-06-11 22:42:51 +0200

    Add GPLv3

--- Platforms/platforms.mk
+++ Platforms/platforms.mk
@@ -1,3 +1,20 @@
+# This file is part of AUSBEE.
+#
+# Copyright (C) 2015 AUSBEE Developers <ausbee@googlegroups.com>
+#
+# AUSBEE is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# AUSBEE is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with AUSBEE.  If not, see <http://www.gnu.org/licenses/>.
+
 ifeq ($(CONFIG_PLATFORM_AUSBEE_MAIN_BOARD_V0_1),y)
 include $(PLATFORMS_PATH)/ausbee_main_board_v0.1/platform.mk
 PLATFORM_CLEAN_GOAL+=platform_ausbee_main_board_v0_1-clean
